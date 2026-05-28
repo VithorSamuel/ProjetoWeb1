55c9b86e961563c708cd8dfc69c6dd849b3ed75f
Vithor <2024332215@ifam.edu.br>
2026-05-27 21:25:36 -0400

    adicionado dropdowns e url cadastro

--- dropdown.js
+++ dropdown.js
@@ -1,17 +1,21 @@
 const btndropdown = document.querySelector(".botao-menu");
-const linkdropdown = document.querySelector(".dropdown-conteudo");
-const dropdown = document.querySelector(".dropdown");
+const linkdropdown = document.querySelectorAll(".dropdown-conteudo");
+const dropdown = document.querySelectorAll(".dropdown");
 if(btndropdown && linkdropdown && dropdown){
+
+dropdown.forEach((dropdown, index) =>{
 dropdown.addEventListener('mouseenter', function(seila){
 seila.stopPropagation();
-linkdropdown.classList.add('show');
+ linkdropdown[index].classList.add('show');
 console.log("click btndropdown");
 });
-
 dropdown.addEventListener('mouseleave', () =>{
-linkdropdown.classList.remove('show');
+linkdropdown[index].classList.remove('show');
 console.log("mouse saiu do linkdropdown");
 });
+});
+
+
 }else{
     console.error("nao apareceu os componentes")
 }
